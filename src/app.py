c043c15e5641258cf0be3f65a7ba23bae333fd92
nalinture <68896641+nalinture@users.noreply.github.com>
2026-05-10 15:37:48 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,1 +1,15 @@
+import streamlit as st
+from transformers import pipeline
 
+st.title("AI Demo using AWS + Hugging Face")
+
+classifier = pipeline(
+    "sentiment-analysis",
+    model="distilbert-base-uncased-finetuned-sst-2-english"
+)
+
+text = st.text_input("Enter text")
+
+if text:
+    result = classifier(text)
+    st.write(result)
